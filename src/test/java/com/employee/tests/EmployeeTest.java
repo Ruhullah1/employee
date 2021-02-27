72a8c6c754b534b40ea46a084d816b2bfd6c38f8
Ruhullah <ruhullah4u@gmail.com>
2021-02-27 16:50:44 +0530

    Few changes made in the code

--- src/test/java/com/employee/tests/EmployeeTest.java
+++ src/test/java/com/employee/tests/EmployeeTest.java
@@ -29,114 +29,155 @@ public void setUp() throws MalformedURLException{
  
    
 	capabilities.setCapability("app", "C:\\Users\\Ruhullah\\Downloads\\app-debug.apk");
-	//capabilities.setCapability("appPackage", "com.aaks.qaautomation2");
-   
-// This package name of your app (you can get it from apk info app)
-	//capabilities.setCapability("appActivity","com.aaks.qaautomation.MainActivity"); // This is Launcher activity of your app (you can get it from apk info app)
-//Create RemoteWebDriver instance and connect to the Appium server
- //It will launch the Calculator App in Android Device using the configurations specified in Desired Capabilities
-   driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 }
 
 
-//@Test
-//public void employeeFormValidation() throws Exception {
-//   //locate the Text on the calculator by using By.name()
-//   WebElement addEmpElement=driver.findElement(By.xpath("//android.widget.ImageButton[contains(@resource-id,'fab')]"));
-//   addEmpElement.click();
-//   
-//   driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//
-//
-//   WebElement createButtonElement=driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id,'createButton')]"));
-//   createButtonElement.click();
-//   
-//   WebElement firstNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationFirstNameTextView')]"));
-//   
-//   WebElement lastNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationLastNameTextView')]"));
-//   
-//   WebElement jobTitleValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationTitleTextView')]"));
-//   
-//   WebElement projectValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationProjectTextView')]"));
-//   
-//   assert firstNameValidatorElement.getText().equals("First Name is Required"):"Actual value is : "+firstNameValidatorElement.getText()+" did not match with expected value";
-//
-//   assert lastNameValidatorElement.getText().equals("Last Name is Required"):"Actual value is : "+lastNameValidatorElement.getText()+" did not match with expected value";
-//
-//   assert jobTitleValidatorElement.getText().equals("Position is Required"):"Actual value is : "+jobTitleValidatorElement.getText()+" did not match with expected value";
-//
-//   assert projectValidatorElement.getText().equals("Project is Required"):"Actual value is : "+projectValidatorElement.getText()+" did not match with expected value";
-//
-//}
-//
-//
-//@Test
-//public void employeeFormValidation2() throws Exception {
-//   //locate the Text on the calculator by using By.name()
-//   WebElement addEmpElement=driver.findElement(By.xpath("//android.widget.ImageButton[contains(@resource-id,'fab')]"));
-//   addEmpElement.click();
-//   
-//   driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//
-//
-//   WebElement createButtonElement=driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id,'createButton')]"));
-//   createButtonElement.click();
-//   
-//   WebElement firstNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationFirstNameTextView')]"));
-//   
-//   WebElement lastNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationLastNameTextView')]"));
-//   
-//   WebElement jobTitleValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationTitleTextView')]"));
-//   
-//   WebElement projectSpinnerElement=driver.findElement(By.xpath("//android.widget.Spinner[contains(@resource-id,'projectSpinner')]"));
-//   
-//   new Select(projectSpinnerElement).selectByIndex(1);
-//   
-//   WebElement projectValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationProjectTextView')]"));
-//   
-//   
-//   assert firstNameValidatorElement.getText().equals("First Name is Required"):"Actual value is : "+firstNameValidatorElement.getText()+" did not match with expected value";
-//
-//   assert lastNameValidatorElement.getText().equals("Last Name is Required"):"Actual value is : "+lastNameValidatorElement.getText()+" did not match with expected value";
-//
-//   assert jobTitleValidatorElement.getText().equals("Position is Required"):"Actual value is : "+jobTitleValidatorElement.getText()+" did not match with expected value";
-//
-//   assert projectValidatorElement.getText().equals(""):"Actual value is : "+projectValidatorElement.getText()+" did not match with expected value";
-//
-//}
+@Test
+public void employeeFormValidation() throws Exception {
+   //locate the Text on the calculator by using By.name()
+   WebElement addEmpElement=driver.findElement(By.xpath("//android.widget.ImageButton[contains(@resource-id,'fab')]"));
+   addEmpElement.click();
+   
+   driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+
+   WebElement createButtonElement=driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id,'createButton')]"));
+   createButtonElement.click();
+   
+   WebElement firstNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationFirstNameTextView')]"));
+   
+   WebElement lastNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationLastNameTextView')]"));
+   
+   WebElement jobTitleValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationTitleTextView')]"));
+   
+   WebElement projectValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationProjectTextView')]"));
+   
+   assert firstNameValidatorElement.getText().equals("First Name is Required"):"Actual value is : "+firstNameValidatorElement.getText()+" did not match with expected value";
+
+   assert lastNameValidatorElement.getText().equals("Last Name is Required"):"Actual value is : "+lastNameValidatorElement.getText()+" did not match with expected value";
+
+   assert jobTitleValidatorElement.getText().equals("Position is Required"):"Actual value is : "+jobTitleValidatorElement.getText()+" did not match with expected value";
+
+   assert projectValidatorElement.getText().equals("Project is Required"):"Actual value is : "+projectValidatorElement.getText()+" did not match with expected value";
+
+}
+
+
+@Test
+public void employeeFormValidation2() throws Exception {
+   //locate the Text on the calculator by using By.name()
+   WebElement addEmpElement=driver.findElement(By.xpath("//android.widget.ImageButton[contains(@resource-id,'fab')]"));
+   addEmpElement.click();
+   
+   driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+
+   WebElement createButtonElement=driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id,'createButton')]"));
+   createButtonElement.click();
+   
+   WebElement firstNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationFirstNameTextView')]"));
+   
+   WebElement lastNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationLastNameTextView')]"));
+   
+   WebElement jobTitleValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationTitleTextView')]"));
+   
+   WebElement projectSpinnerElement=driver.findElement(By.xpath("//android.widget.Spinner[contains(@resource-id,'projectSpinner')]"));
+   
+   new Select(projectSpinnerElement).selectByIndex(1);
+   
+   WebElement projectValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationProjectTextView')]"));
+   
+   
+   assert firstNameValidatorElement.getText().equals("First Name is Required"):"Actual value is : "+firstNameValidatorElement.getText()+" did not match with expected value";
+
+   assert lastNameValidatorElement.getText().equals("Last Name is Required"):"Actual value is : "+lastNameValidatorElement.getText()+" did not match with expected value";
+
+   assert jobTitleValidatorElement.getText().equals("Position is Required"):"Actual value is : "+jobTitleValidatorElement.getText()+" did not match with expected value";
+
+   assert projectValidatorElement.getText().equals(""):"Actual value is : "+projectValidatorElement.getText()+" did not match with expected value";
+
+}
 
 @Test
 public void deleteEmployeeTest() {
 	
-    WebElement addEmpElement=driver.findElement(By.xpath("//android.widget.ImageButton[contains(@resource-id,'fab')]"));
-//  addEmpElement.click();
-//  
-//  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//
-//
-//  WebElement createButtonElement=driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id,'createButton')]"));
-//  createButtonElement.click();
-//  
-//  WebElement firstNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationFirstNameTextView')]"));
-//  
-//  WebElement lastNameValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationLastNameTextView')]"));
-//  
-//  WebElement jobTitleValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationTitleTextView')]"));
-//  
-//  WebElement projectSpinnerElement=driver.findElement(By.xpath("//android.widget.Spinner[contains(@resource-id,'projectSpinner')]"));
-//  
-//  new Select(projectSpinnerElement).selectByIndex(1);
-//  
-//  WebElement projectValidatorElement=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'validationProjectTextView')]"));
-//  
-//  
-//  assert firstNameValidatorElement.getText().equals("First Name is Required"):"Actual value is : "+firstNameValidatorElement.getText()+" did not match with expected value";
-//
+    WebElement firstEmployee=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'fullNameTextView')]"));
+    firstEmployee.click();
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-	
+    WebElement deleteButtonElement=driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id,'deleteEmployeeButton')]"));
+    deleteButtonElement.click();
+    
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    
+    WebElement firstEmployee2=driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'fullNameTextView')]"));
+    
+    assert firstEmployee2.getText().equals("Carl Pavano"):"Actual value is : "+firstEmployee2.getText()+" did not match with expected value";
 	
 }
 
+
+@Test
+public void advertisementAppearTest() {
+	
+	int userCount = 1;
+	int noOfUsers =2;
+	for(int i =0;i<noOfUsers;i++) {
+		this.addUser(userCount);
+		userCount++;
+	}
+	
+	//and verify the advertisement
+}
+
+
+@Test
+public void multipleUsersAdditionAndDeletion() {
+	
+	int userCount = 1;
+	int noOfUsers =10;
+	for(int i =0;i<noOfUsers;i++) {
+		this.addUser(userCount);
+		userCount++;
+	}
+	//delete user
+	//dismis adds
+}
+
+
+public void addUser(int userId) {
+	WebElement addEmpElement=driver.findElement(By.xpath("//android.widget.ImageButton[contains(@resource-id,'fab')]"));
+    addEmpElement.click();
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+	WebElement firstNameElement=driver.findElement(By.xpath("//android.widget.EditText[contains(@resource-id,'firstNameEditText')]"));
+  
+	firstNameElement.sendKeys("firstname-"+userId);
+    WebElement lastNameElement=driver.findElement(By.xpath("//android.widget.EditText[contains(@resource-id,'lastNameEditText')]"));
+    lastNameElement.sendKeys("lastname-"+userId);
+    WebElement jobTitleElement=driver.findElement(By.xpath("//android.widget.Spinner[contains(@resource-id,'titleSpinner')]"));
+  
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	
+	jobTitleElement.click();
+	
+	jobTitleElement.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'text1')]")).click();
+    
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    WebElement projectElement=driver.findElement(By.xpath("//android.widget.Spinner[contains(@resource-id,'projectSpinner')]"));
+    
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    projectElement.findElement(By.xpath("//android.widget.TextView[contains(@resource-id,'text1')]")).click();
+    
+    WebElement createButtonElement=driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id,'createButton')]"));
+    createButtonElement.click();
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+}
 
 @AfterTest
 public void teardown(){
